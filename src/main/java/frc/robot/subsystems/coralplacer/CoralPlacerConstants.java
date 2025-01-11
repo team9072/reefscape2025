ae8790463aa47e3eac6fa2a974ed56e616449cc5
TigerBots <admin@9072.team>
2025-01-11 13:42:01 -0500

    set coralplacer can ids

--- src/main/java/frc/robot/subsystems/coralplacer/CoralPlacerConstants.java
+++ src/main/java/frc/robot/subsystems/coralplacer/CoralPlacerConstants.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.coralplacer;
 import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.units.measure.Voltage;
+import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.generic.roller.RollerConstants;
 import frc.robot.util.CanID;
 
@@ -12,12 +13,12 @@ public class CoralPlacerConstants {
   public static final Voltage extendVoltage = Volts.of(3);
   public static final Voltage retractVoltage = Volts.of(-3);
 
-  public static final String canBus = "";
+  public static final String canBus = TunerConstants.kCANBus.getName();
 
   public static final int beamBreakDioId = 0;
 
-  public static final CanID leftCanId = new CanID(0, canBus);
-  public static final CanID rightCanId = new CanID(0, canBus);
+  public static final CanID leftCanId = new CanID(10, canBus);
+  public static final CanID rightCanId = new CanID(9, canBus);
 
   public static final RollerConstants leftRoller = new RollerConstants(leftCanId);
   public static final RollerConstants rightRoller = new RollerConstants(rightCanId);
