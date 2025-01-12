157bb73696a3d3517fd8a09adb86a3b5e88b0020
TigerBots <admin@9072.team>
2025-01-11 19:10:06 -0500

    add elevator pid and stuff

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems.elevator;
 
 import static edu.wpi.first.units.Units.Amps;
-import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Kilograms;
+import static edu.wpi.first.units.Units.Rotations;
 import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.units.measure.Angle;
@@ -17,8 +17,10 @@ import frc.robot.util.CanID;
 public class ElevatorConstants {
   public static Current currentLimit = Amps.of(40);
 
-  public static Angle minDistance = Degrees.zero();
-  public static Angle maxDistance = Degrees.of(0);
+  public static Angle minDistance = Rotations.of(0);
+  public static Angle maxDistance = Rotations.of(5.6);
+
+  public static double kP = 100;
 
   public static final Mass elevatorMass = Kilograms.zero();
   public static final Distance drumRadius = Inches.of(1.432 / 2);
