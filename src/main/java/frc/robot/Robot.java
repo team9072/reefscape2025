f9b7b2c3c839f512b05367964b1ab7f758629694
TigerBots <admin@9072.team>
2025-01-24 17:05:50 -0500

    remove non drive subsystem constructions

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,37 +9,19 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.Autos;
 import frc.robot.commands.DriveCommands;
 import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.coralplacer.CoralPlacer;
-import frc.robot.subsystems.coralplacer.CoralPlacerConstants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.GyroIO;
 import frc.robot.subsystems.drive.GyroIOPigeon2;
 import frc.robot.subsystems.drive.ModuleIO;
 import frc.robot.subsystems.drive.ModuleIOSim;
 import frc.robot.subsystems.drive.ModuleIOTalonFX;
-import frc.robot.subsystems.elevator.Elevator;
-import frc.robot.subsystems.elevator.ElevatorConstants.ElevatorPosition;
-import frc.robot.subsystems.elevator.ElevatorIO;
-import frc.robot.subsystems.elevator.ElevatorIOSim;
-import frc.robot.subsystems.elevator.ElevatorIOTalonFX;
-import frc.robot.subsystems.generic.beambreak.BeamBreakIO;
-import frc.robot.subsystems.generic.beambreak.BeamBreakIODio;
-import frc.robot.subsystems.generic.beambreak.BeamBreakIONull;
-import frc.robot.subsystems.generic.roller.RollerIO;
-import frc.robot.subsystems.generic.roller.RollerIOSim;
-import frc.robot.subsystems.generic.roller.RollerIOTalonFX;
-import frc.robot.subsystems.vision.Vision;
-import frc.robot.subsystems.vision.VisionConstants.CameraData;
-import frc.robot.subsystems.vision.VisionIO;
-import frc.robot.subsystems.vision.VisionIOPhotonVision;
-import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
 
 public class Robot {
   // Subsystems
   private final Drive drive;
-  private final Vision vision;
+  /*private final Vision vision;
   private final CoralPlacer coralPlacer;
-  private final Elevator elevator;
+  private final Elevator elevator;*/
 
   // Controller
   private final CommandXboxController controller = new CommandXboxController(0);
@@ -59,7 +41,7 @@ public class Robot {
                 new ModuleIOTalonFX(TunerConstants.BackLeft),
                 new ModuleIOTalonFX(TunerConstants.BackRight));
 
-        vision =
+        /*vision =
             new Vision(
                 drive::addVisionMeasurement, new VisionIOPhotonVision(CameraData.LeftCamera));
 
@@ -69,7 +51,7 @@ public class Robot {
                 new RollerIOTalonFX(CoralPlacerConstants.rightRoller),
                 new BeamBreakIODio(CoralPlacerConstants.beamBreakDioId));
 
-        elevator = new Elevator(new ElevatorIOTalonFX());
+        elevator = new Elevator(new ElevatorIOTalonFX());*/
       }
 
       case SIM -> {
@@ -82,7 +64,7 @@ public class Robot {
                 new ModuleIOSim(TunerConstants.BackLeft),
                 new ModuleIOSim(TunerConstants.BackRight));
 
-        vision =
+        /*vision =
             new Vision(
                 drive::addVisionMeasurement,
                 new VisionIOPhotonVisionSim(CameraData.LeftCamera, drive::getPose));
@@ -93,7 +75,7 @@ public class Robot {
                 new RollerIOSim(CoralPlacerConstants.rightRoller),
                 new BeamBreakIONull());
 
-        elevator = new Elevator(new ElevatorIOSim());
+        elevator = new Elevator(new ElevatorIOSim());*/
       }
 
       default -> {
@@ -106,11 +88,11 @@ public class Robot {
                 new ModuleIO() {},
                 new ModuleIO() {});
 
-        vision = new Vision(drive::addVisionMeasurement, new VisionIO() {}, new VisionIO() {});
+        /*vision = new Vision(drive::addVisionMeasurement, new VisionIO() {}, new VisionIO() {});
 
         coralPlacer = new CoralPlacer(new RollerIO() {}, new RollerIO() {}, new BeamBreakIO() {});
 
-        elevator = new Elevator(new ElevatorIO() {});
+        elevator = new Elevator(new ElevatorIO() {});*/
       }
     }
 
@@ -127,7 +109,7 @@ public class Robot {
             () -> -controller.getLeftX(),
             () -> -controller.getRightX()));
 
-    controller.a().whileTrue(coralPlacer.extend());
+    /*controller.a().whileTrue(coralPlacer.extend());
 
     controller
         .b()
@@ -137,7 +119,7 @@ public class Robot {
                 .andThen(coralPlacer.intake().onlyWhile(controller.b())));
 
     controller.rightBumper().onTrue(elevator.setPosition(ElevatorPosition.reefL2Position));
-    controller.leftBumper().onTrue(elevator.setPosition(ElevatorPosition.reefL3Position));
+    controller.leftBumper().onTrue(elevator.setPosition(ElevatorPosition.reefL3Position));*/
 
     // Reset gyro to 0° when start button is pressed
     controller.start().onTrue(DriveCommands.zeroGyro(drive).ignoringDisable(true));
