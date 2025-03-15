28fe60350780a325d8f652c34b6844c5bf1705d3
TigerBots <admin@9072.team>
2025-03-15 12:52:02 -0400

    remove position normalize entirely

--- src/main/java/frc/robot/subsystems/coralplacer/CoralPlacerIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/coralplacer/CoralPlacerIOTalonFX.java
@@ -13,7 +13,6 @@
 
 package frc.robot.subsystems.coralplacer;
 
-import static edu.wpi.first.units.Units.Rotations;
 import static frc.robot.util.PhoenixUtil.*;
 
 import com.ctre.phoenix6.BaseStatusSignal;
@@ -29,7 +28,6 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
-import edu.wpi.first.units.measure.MutAngle;
 import edu.wpi.first.units.measure.Voltage;
 import frc.robot.subsystems.coralplacer.CoralPlacerConstants.CoralPlacerPosition;
 
@@ -42,8 +40,6 @@ public class CoralPlacerIOTalonFX implements CoralPlacerIO {
   private final StatusSignal<AngularVelocity> velocityRotPerSec;
   private final StatusSignal<Voltage> appliedVolts;
   private final StatusSignal<Current> currentAmps;
-
-  private MutAngle offsetRotations = Rotations.mutable(0);
 
   private final MotionMagicVoltage positionRequest =
       new MotionMagicVoltage(CoralPlacerPosition.stowPosition.angle);
@@ -89,7 +85,7 @@ public class CoralPlacerIOTalonFX implements CoralPlacerIO {
   public void updateInputs(CoralPlacerInputs inputs) {
     BaseStatusSignal.refreshAll(positionRot, velocityRotPerSec, appliedVolts, currentAmps);
 
-    inputs.position = positionRot.getValue().minus(offsetRotations);
+    inputs.position = positionRot.getValue();
     inputs.velocity = velocityRotPerSec.getValue();
     inputs.appliedVoltage = appliedVolts.getValue();
     inputs.current = currentAmps.getValue();
@@ -97,10 +93,6 @@ public class CoralPlacerIOTalonFX implements CoralPlacerIO {
 
   @Override
   public void setPosition(CoralPlacerPosition positon) {
-    motor.setControl(positionRequest.withPosition(positon.angle.plus(offsetRotations)));
-  }
-
-  public void normalizePosition() {
-    offsetRotations.mut_replace(Math.floor(positionRot.getValue().in(Rotations)), Rotations);
+    motor.setControl(positionRequest.withPosition(positon.angle));
   }
 }
