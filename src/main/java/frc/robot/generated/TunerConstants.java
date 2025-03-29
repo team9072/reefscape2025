de563c5e1ca03405be0a8fa02de3f2455bc73451
TigerBots <admin@9072.team>
2025-03-29 10:35:08 -0400

    fix encoder

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -164,7 +164,7 @@ public class TunerConstants {
   private static final int kBackLeftDriveMotorId = 5;
   private static final int kBackLeftSteerMotorId = 6;
   private static final int kBackLeftEncoderId = 3;
-  private static final Angle kBackLeftEncoderOffset = Rotations.of(-0.0322265625);
+  private static final Angle kBackLeftEncoderOffset = Rotations.of(-0.025634765625);
   private static final boolean kBackLeftSteerMotorInverted = true;
   private static final boolean kBackLeftEncoderInverted = false;
 
