f23f4375db091174ccc529e943c3c2158dd6e6a8
TigerBots <admin@9072.team>
2025-04-05 08:28:15 -0400

    Put L2 and L3 align closer to reef

--- src/main/java/frc/robot/commands/ReefAlignment.java
+++ src/main/java/frc/robot/commands/ReefAlignment.java
@@ -21,7 +21,7 @@ import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
 public class ReefAlignment {
-  private static final Distance reefLowerOffsetDistance = Inches.of(7.75);
+  private static final Distance reefLowerOffsetDistance = Inches.of(7.5);
   private static final Distance reefL4OffsetDistance = Inches.of(8.25);
   private static final Distance reefAlgaeOffsetDistance = Inches.of(15);
 
